1e8ee5a3ab25c8d5ef4bf318302106a79784b7b2
sosugyeong <sugyeong247@gmail.com>
2026-05-04 16:38:55 +0900

    Initial commit

---
+++ src/main/java/com/example/coffeeorderproject/CoffeeOrderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeorderproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOrderProjectApplication.class, args);
+    }
+
+}
