ba30337c3ed3938c2086a307208806854fbd0f10
sosugyeong <sugyeong247@gmail.com>
2026-05-06 18:38:15 +0900

    feat: Member 에러코드 추가

--- src/main/java/com/example/coffeeorderproject/global/exception/ErrorCode.java
+++ src/main/java/com/example/coffeeorderproject/global/exception/ErrorCode.java
@@ -11,9 +11,11 @@ public enum ErrorCode {
     // ===== SERVER (S) =====
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"S001","서버 내부 오류가 발생했습니다."),
 
+    // ===== MENU (M1) =====
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "M101", "존재하지 않는 유저입니다."),
 
     // ===== MENU (M) =====
-    MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "C001", "존재하지 않는 메뉴입니다."),
+    MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "M001", "존재하지 않는 메뉴입니다."),
 
     // ===== POINT (P) =====
     INSUFFICIENT_POINT(HttpStatus.BAD_REQUEST, "P001", "포인트 잔액이 충분하지 않습니다."),
